33afc1c68930ba175823c66284138944932b6eea
blwky <kimbriancaronan0@gmail.com>
2018-10-26 00:19:27 +0800

    Used python format strings'

---
+++ Caronan_e2.py
@@ -0,0 +1,18 @@
+import sys
+Sustained_Winds = sys.argv[]
+
+if sustained winds >= 220:
+    print ("Super Typhoon")
+elif sustained winds >= 118:
+    print ("Typhoon")
+elif sustained winds >= 89:
+    print ("Severe Tropical Typhoon")
+elif sustained winds >= 62:
+    print ("Tropical Storm")
+else sustained winds >= 61"
+    print ("Tropical Depression")
+
+
+
+
+
--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 print ("Hello, World")
 print ("My Name is {}. I am {} y/o. " \
-.format("Kib",16))
-print("AdnU", 2018, sep="-", end="")
+.format("Kib",18))
+print("AdnU", 2018, sep= end="")
 print("CSNHS")
