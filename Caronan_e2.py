7257b8ba5943ccd0bdfa632009b89265ba6c73cc
blwky <kimbriancaronan0@gmail.com>
2018-10-26 00:28:48 +0800

    Used python format strings'

--- Caronan_e2.py
+++ Caronan_e2.py
@@ -1,6 +1,21 @@
 import sys
 Sustained_Winds = sys.argv[]
 
+
+Category
+Sustained Winds
+Super Typhoon
+≥220 km/h
+Typhoon
+118–220 km/h
+Severe Tropical Storm
+89–117 km/h
+Tropical Storm
+62–88 km/h
+Tropical Depression
+≤61 km/h
+
+
 if sustained winds >= 220:
     print ("Super Typhoon")
 elif sustained winds >= 118:
